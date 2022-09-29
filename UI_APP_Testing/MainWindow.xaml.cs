929e724e50b933518def1378fa99330583d0bec5
Nirmal Kumar Sekar <nxsekar1@shockers.wichita.edu>
2022-09-29 12:15:26 -0500

    ComboBox changes

--- UI_APP_Testing/MainWindow.xaml.cs
+++ UI_APP_Testing/MainWindow.xaml.cs
@@ -20,14 +20,33 @@ namespace UI_APP_Testing
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Person> people = new List<Person>();
         public MainWindow()
         {
             InitializeComponent();
+
+            people.Add(new Person { FirstName = "nirmal", LastName = "kumar" });
+            people.Add(new Person { FirstName = "Sekar", LastName = "Dh" });
+
+            mycomboBox.ItemsSource = people;
         }
 
         private void sumbitButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show($"Hi {firstName.Text} My first presentation");
+        }
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string FullName
+            {
+                get
+                {
+                    return $"{FirstName} {LastName}";
+                }
+            } 
         }
     }
 }
